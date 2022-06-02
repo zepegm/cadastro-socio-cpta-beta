c33f188fd936ae529382e217bce7f5335b1d676e
zepegm <zepegm@gmail.com>
2022-06-02 11:38:54 -0300

    Update cep.py

--- cep.py
+++ cep.py
@@ -1,2 +1,10 @@
 from pycep_correios import get_address_from_cep, WebService
 
+def retornarCEP(cep):
+
+    try:
+        address = get_address_from_cep(cep, webservice=WebService.APICEP)
+    except:
+        address = {'bairro': '', 'cep': '12630-000', 'cidade': 'Cachoeira Paulista', 'logradouro': '', 'uf': 'SP', 'complemento': ''}
+
+    return address
