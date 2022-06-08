95836638f58effdf68a4f0802ea5c01e6ee78f42
zepegm <zepegm@gmail.com>
2022-06-08 17:22:56 -0300

    Update testes.py

--- testes.py
+++ testes.py
@@ -1,3 +1,8 @@
-from cep import retornarCEP
+from conexaoBD import Conexao
 
-print(retornarCEP("12605500"))
+cred = {'HOST':'ec2-34-197-84-74.compute-1.amazonaws.com', 'PORT':5432, 'USER':'piendkrxzduecj', 'PASSWORD':'5af529088848fd2537e0c97f22469dc181a79b2a1cfcc98de6959275e13cab3d', 'DB':'do5a2tog3uf07'}
+
+banco = Conexao(cred)
+
+print(banco.consultar("Select * from cidadao"))
+
